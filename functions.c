3f2f0343dbf7e51f26f309c762f22d62d7d4b898
Izzohpro <okparafrankline@gmail.com>
2023-04-25 13:34:47 +0100

    Comment edited by Franklin

--- functions.c
+++ functions.c
@@ -9,7 +9,8 @@
  * @flags:  Calculates active flags
  * @width: Width
  * @precision: Precision specification
- * @size: Size specifier
+ * @size: Size specifier 
+ * project by team Jude and Franklin
  * Return: Number of chars printed
  */
 int print_char(va_list types, char buffer[],
