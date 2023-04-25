c21da84f671012c05600d7e71fae830d82082c4f
Izzohpro <okparafrankline@gmail.com>
2023-04-25 13:53:18 +0100

    Comment edited by Franklin

--- utils.c
+++ utils.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
+ * is_printable - Evaluates if a char is printable 
+ * project by team Jude and Franklin
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
