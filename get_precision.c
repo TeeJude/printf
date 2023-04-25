cca6500f14c64f8bf66f7343aa92fcdad68ba56e
Izzohpro <okparafrankline@gmail.com>
2023-04-25 13:42:38 +0100

    Comment edited by Franklin

--- get_precision.c
+++ get_precision.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
+ * get_precision - Calculates the precision for printing 
+ * project by team Jude and Franklin
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
