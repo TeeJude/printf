81fcc5253d0c391b43af38fc845788a4bfe13f26
Izzohpro <okparafrankline@gmail.com>
2023-04-25 13:45:12 +0100

    Comment edited by Franklin

--- get_size.c
+++ get_size.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
+ * get_size - Calculates the size to cast the argument 
+ * project by team Jude and Franklin
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
